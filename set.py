d6d8c71d03cf4dc5a83e2a8302819adff1ad251c
Hardi9 <97654949+Hardi9@users.noreply.github.com>
2022-01-15 13:05:23 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,44 @@
+# 20ce126
+# Hardi Shah
+# A: Write a Python program to add member(s) in a set and clear a set
+
+set = {'A', 'B', 'C', 'D', 'E'}
+print("set before adding :",set)
+set.add('F')
+print("Set before clear:", set)
+print("Set after clear:", set.clear())
+
+# B:Write a Python program to remove an item from a set if it is present in the set.
+
+set = {'A', 'B', 'C', 'D', 'E'}
+set.discard('A');
+print(set)
+
+#C:Write a Python program to create an intersection, Union, difference of sets.
+
+setA = {1, 2, 3, 4, 5}
+setB = {4, 5, 6, 7, 8}
+print("Union of A and B:", setA.union(setB))
+print("Intersection of A and B:", setA.intersection(setB))
+print("Difference of A and B:", setA.difference(setB))
+
+# D:Write a Python program to find maximum and the minimum value in a set.
+set={1,2,3,4,5,6,7,8,9,10}
+print("maxium of set :",max(set))
+print("minium of set :",min(set))
+
+#E:Write a Python program to find the most common elements and their counts from list, tuple, dictionary.
+
+list1 = [12,8,3.4,5.6,20]
+tuple1 = (12,20,10,3.4,2)
+dictionary1 = {20,1,3.4,5.6}
+print("List =", list1)
+print("Tuple =", tuple1)
+print("Dictionary =",dictionary1)
+set1 =set(list1).intersection(set(tuple1))
+result_set = set1.intersection(set(dictionary1))
+final_list = list(result_set)
+print("Common of members of list, tuple and dictionary:", final_list)
+
+    
+    
---
+++ tuple.py
@@ -0,0 +1,26 @@
+# 20ce126
+# Hardi Shah
+# A: a. Write a Python program to create a tuple with different data types.
+tuple1 = (1, 2, 3, 4)
+tuple2 = ("Hello", "World")
+print(tuple1)
+print(tuple2)
+
+#B: Write a Python program to create a tuple with numbers and print one item.
+tuple1 = (1, 2, 3, 4)
+print(tuple1[3])
+
+#C:Write a Python program to add an item in a tuple.
+tuple = ('P','Y','T','H','O')
+print(tuple)
+tuple= tuple + ('N',)
+print(tuple)
+
+#D: Write a Python program to convert a tuple to a string.
+tuple = ('H', 'e', 'l', 'l', 'o' ,)
+str = ''.join(tuple)
+print(str)
+
+#E:Write a Python program to find the length of a tuple.
+tuple = ('H', 'e', 'l', 'l', 'o')
+print("Length of the tuple:", len(tuple))
